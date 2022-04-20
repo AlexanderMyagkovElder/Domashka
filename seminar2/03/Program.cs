70501647775bc04adb277bd55ef9c8c848eeb3e3
Alexander Myagkov <anistonbale@gmail.com>
2022-04-20 22:58:02 +0300

    zadacha 3 week reshenie

--- seminar2/03/Program.cs
+++ seminar2/03/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+int[] week = { 1, 2, 3, 4, 5, 6, 7 };
+Console.Write("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day <= 0 || day > 7) 
+{
+    Console.WriteLine("В неделе 7 дней! Будьте внимательны...");
+}
+else
+{
+    if (week[day - 1] < 6)
+    {
+    Console.WriteLine("нет, это не выходной");
+    }
+    else
+    {
+    Console.WriteLine("да, это выходной");
+    }
+} 
+
+
