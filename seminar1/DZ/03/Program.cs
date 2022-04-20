f38fdf9e6f3a32535274c9e781a9ef07f4f0ff4a
Alexander Myagkov <anistonbale@gmail.com>
2022-04-20 23:05:02 +0300

    dom 1

---
+++ seminar1/DZ/03/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным.
+Console.Write("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число " + a + " кратное");
+}
+else
+{
+    Console.WriteLine("Число " + a + " некратное");
+}
