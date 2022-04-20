4d5278b6fb32d0e468930e9cd32e7426d3ea8e1f
Alexander Myagkov <anistonbale@gmail.com>
2022-04-20 21:47:35 +0300

    add zadacha2 reshenie

--- seminar2/02/Program.cs
+++ seminar2/02/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Введите число: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+if (chislo<100)
+{
+    Console.WriteLine("Число не содержит третью цифру. Попробуй другое.");
+} 
+else
+{
+    string massive = Convert.ToString(chislo);
+    Console.WriteLine(massive[2]);
+}
