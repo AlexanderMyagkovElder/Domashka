fc432388ef71b3d0e8dd4248e817e36f3b54d2e5
Alexander Myagkov <anistonbale@gmail.com>
2022-04-24 22:35:24 +0300

    reshenie zada4i co stepenyami

---
+++ seminar4/01/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.*/
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+int stepen=1;
+while (index<=b)
+{
+    stepen=stepen*a;
+    index++;
+}
+Console.WriteLine(stepen);
