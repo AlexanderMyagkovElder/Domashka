66c800303437ca17be0b5063a1c163b5474532b7
Alexander Myagkov <anistonbale@gmail.com>
2022-04-24 21:41:55 +0300

    reshenie c koordinatami

--- seminar3/02/Program.cs
+++ seminar3/02/Program.cs
@@ -12,8 +12,13 @@ Console.Write("Введите координату Y точки B: ");
 int b2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату Z точки B: ");
 int c2 = Convert.ToInt32(Console.ReadLine());
-int katet1 = a2-a1;
-int katet2=b2-b1;
-int katet3=c2-c1;
-double distance = Math.Sqrt(katet1*katet1+katet2*katet2+katet3*katet3);
-Console.WriteLine("Расстояние между двумя точками равно "+distance);
+double Katet(int a, int b)
+{
+    int katet = a - b;
+    return Math.Pow(katet,2);
+}
+double katet1 = Katet(a2, a1);
+double katet2 = Katet(b2, b1);
+double katet3 = Katet(c2, c1);
+double distance = Math.Sqrt(katet1+katet2+katet3);
+Console.WriteLine("Расстояние между двумя точками равно " + distance);
