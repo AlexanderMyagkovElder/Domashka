8647ee7acdcb1a752db946853bceb37447e4dfcc
Alexander Myagkov <anistonbale@gmail.com>
2022-04-24 22:01:56 +0300

    reshenie kubov

--- seminar3/03/Program.cs
+++ seminar3/03/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.*/
 Console.Write("Введите число N: ");
-double N = Convert.ToDouble(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
 if (N<0) N=-N;
 int index = 1;
 while (index<=N)
