087586c5025459b1fcf99fe23dd3080d20513594
Alexander Myagkov <anistonbale@gmail.com>
2022-05-04 19:50:49 +0300

    zadacha nezadacha

---
+++ seminar4/02/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11  82 -> 10   9012 -> 12*/
+Console.Write("Введите число: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+string chisloMassive = Convert.ToString(chislo);
+int sum = 0;
+for (int i = 0; i < chisloMassive.Length; i++)
+{
+    sum = sum + Convert.ToInt32(chisloMassive[i].ToString());
+}
+Console.WriteLine(sum);
