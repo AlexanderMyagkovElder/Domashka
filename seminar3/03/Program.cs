47d53c63dfc07cf565c5d23290904c1b56e02f61
Alexander Myagkov <anistonbale@gmail.com>
2022-05-04 14:51:10 +0300

    пришло осознание)

--- seminar3/03/Program.cs
+++ seminar3/03/Program.cs
@@ -1,14 +1,15 @@
 ﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.*/
 Console.Write("Введите число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-if (N<0) N=-N;
+if (N < 0) N = -N;
 int index = 1;
-while (index<=N)
+while (index <= N)
 {
-    double cube=Cube(index,3);
+    double cube = Cube(index, 3);
     index++;
-    Console.Write(cube+"  ");
+    Console.Write(cube + "  ");
 }
+/*по сути эта часть и не нужна, получается*/
 double Cube(int a, int b)
 {
     double cube=Math.Pow(a,b);
