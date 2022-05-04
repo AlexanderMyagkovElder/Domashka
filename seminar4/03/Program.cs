d578540c38e792e1b07e5d9d3cd83483ef4ae118
Alexander Myagkov <anistonbale@gmail.com>
2022-05-04 21:27:43 +0300

    задача не решена по сути

---
+++ seminar4/03/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
+6, 1, 33 -> [6, 1, 33] */
+
+Console.WriteLine("Введите ряд чисел через запятую: ");
+string vvodimoe = Console.ReadLine();
+Console.Write("{");
+for (int i = 0; i < vvodimoe.Length; i++)
+{
+    Console.Write(vvodimoe[i]);
+}
+Console.Write("}");
