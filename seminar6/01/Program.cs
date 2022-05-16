1194ad93cc94a3227eec395e41fa29e9c1548362
Alexander Myagkov <anistonbale@gmail.com>
2022-05-16 19:08:15 +0300

    reshenie 1 zadachi

---
+++ seminar6/01/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+Console.Write("Введите количество чисел (М): ");
+int length=Convert.ToInt32(Console.ReadLine());
+int[] massive=new int[length];
+for (int i = 0; i < massive.Length; i++)
+{
+    Console.Write($"Введите {i+1} число: ");
+    massive[i]=Convert.ToInt32(Console.ReadLine());
+}
+int sum=0;
+for (int i = 0; i < massive.Length; i++)
+{
+    if (massive[i]>0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine(sum);
